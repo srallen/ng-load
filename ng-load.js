32d2f87c0713f08cea239eadf3224aa89da79cf5
Sarah Allen <sarah@zooniverse.org>
2015-11-02 11:22:31 -0600

    Remove restrict

--- ng-load.js
+++ ng-load.js
@@ -5,7 +5,6 @@
     .directive('ngLoad', ['$parse', function($parse){
 
         return {
-            restrict: 'A',
             compile: function($element, attr) {
                 var fn = $parse(attr['ngLoad']);
 
